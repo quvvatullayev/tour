9a129e3edf4a46c03d686a8b1532d9df0c9cef28
ogabek <quvvatullayev@gmail.com>
2023-06-15 15:16:39 +0500

    create exclusive

---
+++ tour/views/exclusive.py
@@ -0,0 +1,54 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from ..models import (
+    Exclusive,
+)
+from ..serializers import (
+    ExclusiveSerializer,
+)
+
+class ExclusiveCreate(APIView):
+    def post(self, request:Request):
+        data = request.data
+        serializer = ExclusiveSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            {
+                'error': serializer.errors,
+                'message': 'Invalid data'
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+class ExclusiveList(APIView):
+    def get(self, request:Request):
+        exclusives = Exclusive.objects.all()
+        serializer = ExclusiveSerializer(exclusives, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class ExclusiveDetail(APIView):
+    def get(self, request:Request, id):
+        exclusive = Exclusive.objects.get(id=id)
+        serializer = ExclusiveSerializer(exclusive)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class ExclusiveUpdate(APIView):
+    def post(self, request:Request, id):
+        data = request.data
+        exclusive = Exclusive.objects.get(id=id)
+        exclusive.name = data.get('name', exclusive.name)
+        exclusive.price = data.get('price', exclusive.price)
+        exclusive.duration = data.get('duration', exclusive.duration)
+        exclusive.save()
+        serializer = ExclusiveSerializer(exclusive)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class ExclusiveDelete(APIView):
+    def post(self, request:Request, id):
+        exclusive = Exclusive.objects.get(id=id)
+        exclusive.delete()
+        return Response({'message': 'Deleted'}, status=status.HTTP_200_OK)
