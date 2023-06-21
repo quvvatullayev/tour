e206b4395d7a2f3049290818de9f85374a7b117c
ogabek <quvvatullayev@gmail.com>
2023-06-21 09:22:35 +0500

    fix swagger settings

--- core/settings.py
+++ core/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "drf_yasg",
     'tour',
     'rest_framework',
-    'drf_yasg',
 ]
 
 MIDDLEWARE = [
