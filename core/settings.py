4ad9bc812d289a231ab0b3571f2064467cc2fee4
ogabek <quvvatullayev@gmail.com>
2023-06-21 09:33:26 +0500

    fix language code

--- core/settings.py
+++ core/settings.py
@@ -94,7 +94,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'uz-uz'
+# english
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
